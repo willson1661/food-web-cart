3ce01081ad9ada8cd38ed1adf428d6ecd62b2403
willson <93517421+willson1661@users.noreply.github.com>
2023-12-18 15:35:54 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+//https://codesandbox.io/s/simple-ecart-gh1wro
